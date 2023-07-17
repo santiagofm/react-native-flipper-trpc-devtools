2ed1b5d7121caa87e6a92e4f6a46aaae55494c85
santiagofm <santiagofermendy@gmail.com>
2023-07-17 00:06:51 -0300

    feat: better example client/server

--- packages/example-client/src/Example.tsx
+++ packages/example-client/src/Example.tsx
@@ -1,21 +1,82 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  Text,
-  ScrollView,
-  StyleSheet,
-  Button,
-} from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+
 import { trpc } from './trpc';
+import { ProcedureCard } from './ProcedureCard';
+import { InputItem, WhiteSpace } from '@ant-design/react-native';
 
 export const Example: React.FC = () => {
-  const { data, refetch } = trpc.greeting.useQuery();
+  const [input1, setInput1] = React.useState('');
+  const { data: greeting, refetch: refetchGreeting } = trpc.greeting.useQuery({
+    name: input1,
+  });
+
+  const { data: nestedGreeting, refetch: refetchNestedGreeting } =
+    trpc.nested.greeting.useQuery();
+
+  const [input2, setInput2] = React.useState('');
+  const { mutateAsync: setGreeting } = trpc.nested.setGreeting.useMutation({
+    onSuccess: refetchNestedGreeting,
+  });
+
+  const [serverTime, setServerTime] = React.useState<string>();
+  const [subscriptionEnabled, setSubscriptionEnabled] = React.useState(true);
+  trpc.time.useSubscription(undefined, {
+    enabled: subscriptionEnabled,
+    onData: data => setServerTime(data),
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
-        <Text>{data?.text}</Text>
-        <Button onPress={() => refetch()} title="Refetch" />
+        <ProcedureCard
+          title="Query"
+          value={greeting?.text}
+          button={{
+            title: 'Refetch',
+            onPress: refetchGreeting,
+          }}>
+          <InputItem
+            clear
+            value={input1}
+            onChange={setInput1}
+            placeholder="Some input"
+            style={styles.input}
+          />
+        </ProcedureCard>
+        <WhiteSpace size="lg" />
+        <ProcedureCard
+          title="Nested Query"
+          value={nestedGreeting?.hello}
+          button={{
+            title: 'Refetch',
+            onPress: refetchNestedGreeting,
+          }}
+        />
+        <WhiteSpace size="lg" />
+        <ProcedureCard
+          title="Mutation"
+          button={{
+            title: 'Set Nested Greeting',
+            onPress: () => setGreeting({ greeting: input2 }),
+          }}>
+          <InputItem
+            clear
+            value={input2}
+            onChange={setInput2}
+            placeholder="Some new greeting"
+            style={styles.input}
+          />
+        </ProcedureCard>
+        <WhiteSpace size="lg" />
+        <ProcedureCard
+          title="Subscription"
+          value={serverTime}
+          button={{
+            title: subscriptionEnabled ? 'Stop' : 'Start',
+            onPress: () => setSubscriptionEnabled(enabled => !enabled),
+          }}
+        />
       </ScrollView>
     </SafeAreaView>
   );
@@ -24,5 +85,10 @@ export const Example: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 4,
   },
 });
--- packages/example-server/index.ts
+++ packages/example-server/index.ts
@@ -1,6 +1,8 @@
 import { initTRPC } from "@trpc/server";
-import { createHTTPServer } from "@trpc/server/adapters/standalone";
-import cors from "cors";
+import { applyWSSHandler } from "@trpc/server/adapters/ws";
+import { observable } from "@trpc/server/observable";
+import EventEmitter from "events";
+import ws from "ws";
 import { z } from "zod";
 
 const PORT = 9999;
@@ -10,7 +12,27 @@ const t = initTRPC.create();
 const publicProcedure = t.procedure;
 const router = t.router;
 
+const ee = new EventEmitter();
+
+let nestedState = "world";
+
+const nested = router({
+  greeting: publicProcedure.query(() => ({
+    hello: nestedState,
+  })),
+  setGreeting: publicProcedure
+    .input(
+      z.object({
+        greeting: z.string(),
+      })
+    )
+    .mutation(({ input }) => {
+      nestedState = input.greeting;
+    }),
+});
+
 const appRouter = router({
+  nested,
   greeting: publicProcedure
     .input(
       z
@@ -24,14 +46,42 @@ const appRouter = router({
         text: `hello ${input?.name ?? "world"}`,
       };
     }),
+  time: t.procedure.subscription(() => {
+    return observable<string>((emit) => {
+      const onTime = (data: string) => {
+        emit.next(data);
+      };
+      ee.on("time", onTime);
+      return () => {
+        ee.off("time", onTime);
+      };
+    });
+  }),
 });
+
+const recurringInterval = setInterval(() => {
+  ee.emit("time", Date.now().toString());
+}, 1500);
 
 export type AppRouter = typeof appRouter;
 
-createHTTPServer({
-  middleware: cors(),
-  router: appRouter,
-  createContext() {
-    return {};
-  },
-}).listen(PORT);
+const wss = new ws.Server({
+  port: PORT,
+});
+const handler = applyWSSHandler({ wss, router: appRouter });
+
+wss.on("connection", (ws) => {
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.once("close", () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
+});
+
+console.log(`✅ WebSocket Server listening on ws://localhost:${PORT}`);
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM");
+  handler.broadcastReconnectNotification();
+  clearInterval(recurringInterval);
+  wss.close();
+});
